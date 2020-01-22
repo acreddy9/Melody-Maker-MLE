e84beed04a2769212272b92c8d9f39b4d5e88604
lredlin <48256408+lredlin@users.noreply.github.com>
2020-01-21 21:41:10 -0600

    Updated so song plays on click
    commented out code for customization to add after GUI is created

--- Composer.java
+++ Composer.java
@@ -1,3 +1,12 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.Sequence;
+import javax.sound.midi.Sequencer;
+
 /**
  * Driver class for Melody Maker application
  * 
@@ -8,18 +17,41 @@ public class Composer {
 
 	public static void main(String[] args) {
 		// TODO: get input values from user, convert to hex
-		setSongLength(42);
-		setKeySignature(7); // G Major/E Minor
-		setTimeSignature(4,4); // common time
-		setTempo(96,4); // 96 quarter notes per minute
-		setDynamicLevel(64); // mezzo forte
+		//setSongLength(42);
+		//setKeySignature(7); // G Major/E Minor
+		//setTimeSignature(4,4); // common time
+		//setTempo(96,4); // 96 quarter notes per minute
+		//setDynamicLevel(64); // mezzo forte
 		
 		// TODO: rhythm? repeated or varies throughout song?
 		// TODO: volume?
 		// TODO: chords, chord progression?
 		// TODO: changing dynamic levels?
-		
-		generateNotes();
+		      try {
+        TrainSet.main(null);
+        midifile.main(null);
+        MLE.main(null);
+        VelocityMLE.main(null);
+        LengthMLE.main(null);
+        MIDIMaker.main(null);
+        try {
+          Sequencer sequencer = MidiSystem.getSequencer(); // Get the default Sequencer
+          if (sequencer==null) {
+              System.err.println("Sequencer device not supported");
+              return;
+          } 
+          sequencer.open(); // Open device
+          // Create sequence, the File must contain MIDI file data.
+          Sequence sequence = MidiSystem.getSequence(new File("song.mid"));
+          sequencer.setSequence(sequence); // load it into sequencer
+          sequencer.start();  // start the playback
+      } catch (MidiUnavailableException | InvalidMidiDataException | IOException ex) {
+          ex.printStackTrace();
+      }
+      }catch(Exception E) {
+        E.printStackTrace();
+      }
+
 	}
 	
 	public static void generateNotes() {
